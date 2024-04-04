777d45962f04ff8c2a9b335cd998adf629c7e22b
reza gorji <rezagorji68@yahoo.com>
2024-04-04 21:50:12 +0330

    Fix bugs and gradle

---
+++ app/src/main/java/com/note/mynote/di/AppConfig.kt
@@ -0,0 +1,7 @@
+package com.note.mynote.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppConfig : Application()
---
+++ app/src/main/java/com/note/mynote/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.note.mynote.di
+
+import android.content.Context
+import androidx.room.Room
+import com.note.mynote.db.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val DB_NAME = "note.db"
+
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context) =
+        synchronized(AppDatabase::class) {
+        Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDao(database: AppDatabase) = database.noteDao()
+}
